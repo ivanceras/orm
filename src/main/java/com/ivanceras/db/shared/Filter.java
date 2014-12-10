811ac6074458d48ddb1c9ef572b56a364de3805c
ivanceras <ivanceras@gmail.com>
2014-12-10 20:34:26 +0800

    remote commented out code

--- src/main/java/com/ivanceras/db/shared/Filter.java
+++ src/main/java/com/ivanceras/db/shared/Filter.java
@@ -15,23 +15,6 @@ import com.ivanceras.keyword.sql.SQL;
  */
 public class Filter {
 	
-//	public static final String LESS_THAN = "<";
-//	public static final String LESS_THAN_OR_EQUAL = "<=";
-//	public static final String EQUAL = "=";
-//	public static final String GREATER_THAN = ">";
-//	public static final String GREATER_THAN_OR_EQUAL = ">=";
-//	public static final String NOT_EQUAL = "!=";
-//	public static final String IN = "IN";
-//	public static final String NOT_IN = "NOT IN";
-//	public static final String LIKE = "LIKE";
-//	public static final String NOT_NULL = "NOT NULL";
-//	public static final String NULL = "NULL";
-//	public static final String IS_NULL = "IS NULL";
-//
-//	private static final String AND = "AND";
-//	private static final String OR = "OR";
-//	public static final String NOT_LIKE = "NOT LIKE";
-	
 	public enum Connector{
 		AND,
 		OR
